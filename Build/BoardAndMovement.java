cedb101bb089b781109e568d8505d3283cad41b5
Mdodson06 <mariadodson8@gmail.com>
2024-04-15 21:11:15 -0500

    Completed setStartSpots

--- Build/BoardAndMovement.java
+++ Build/BoardAndMovement.java
@@ -2,29 +2,16 @@ package Build;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class BoardAndMovement {
     public static ArrayList<int[]> activeSpots = new ArrayList<>();
-    public int[][] direction = new int[6][3];
-
     public static void main(String[] args) {
-        BoardAndMovement bm = new BoardAndMovement();
-        int[] a = {1,2,3};
-        int[] d = {1,2,3};
-        int[] b = {4,5,7};
-        int[] c = {3,3,3};
-        int[] e = {3,3,4};
-
-        activeSpots.add(a);
-        activeSpots.add(b);
-        System.out.println("valid move dbc: " + bm.validMove(d, b, c));
-        d[0] = 1;
-        d[1] = 2;
-        d[2] = 3;
-        System.out.println("valid move dbe: " + bm.validMove(d, b, e));
-        bm.removeSpot(d);
-        bm.removeSpot(c);
-
+        BoardAndMovement bm = new BoardAndMovement();  
+        bm.setStartSpots();
+        for(int[] i : activeSpots){
+            System.out.println(Arrays.toString(i));
+        }
         /*
          * Still to do: 
          * setStartSpots
@@ -35,7 +22,62 @@ public class BoardAndMovement {
 
     public void setStartSpots(){
         activeSpots.clear();
-        
+        Random rnd = new Random();
+        int oneFish = 30;
+        int twoFish = 20;
+        int threeFish = 10;
+        int q = 5;
+        int r = 1;
+        int s = 11;
+        while(activeSpots.size() < 60){
+            int fishChoice = rnd.nextInt(1,7);
+            if((fishChoice == 1 || fishChoice == 2 || fishChoice == 3)&& oneFish > 0){
+                oneFish--;
+                activeSpots.add(new int[] {q,r,s,1});
+            } else if((fishChoice == 4 || fishChoice == 5) && twoFish > 0) {
+                twoFish--;
+                activeSpots.add(new int[] {q,r,s,2});
+            } else if(fishChoice == 6 && threeFish > 0) {
+                threeFish--;
+                activeSpots.add(new int[] {q,r,s,3});
+            } else {
+                continue;
+            }
+            //NOTE: Just hard-coding all the new lines
+            //There is probably a more efficient way of doing this 
+            if(q == 11 && s == 5){
+                q = 4; 
+                s = 11;
+                r++;
+            } else if(q == 11 && s == 4){
+                q = 4; 
+                s = 10;
+                r++;
+            } else if(q == 10 && s == 4){
+                q = 3; 
+                s = 10;
+                r++;
+            } else if(q == 10 && s == 3){
+                q = 3; 
+                s = 9;
+                r++;
+            } else if(q == 9 && s == 3){
+                q = 2; 
+                s = 9;
+                r++;
+            } else if(q == 9 && s == 2){
+                q = 2; 
+                s = 8;
+                r++;
+            } else if(q == 8 && s == 2){
+                q = 1; 
+                s = 8;
+                r++;
+            } else {
+                q++;
+                s--;
+            }
+        }            
     }
 
     //takes two int[] of 3 values and returns an int[] of the direction between the two points
